09214452c652ef2042bc76fc2891792da4339060
Pavan Tikkani <pavan@Pavans-MacBook-Pro.local>
2025-03-04 15:26:56 +0530

    Added custom margins

--- main.py
+++ main.py
@@ -11,25 +11,30 @@ hdrs = (
 
 app, rt = fast_app(hdrs=hdrs, htmlkw={"data-theme": "fantasy"}, pico=False, live=False)
 
+def meditation_button(additional_classes=""):
+    """Helper function to avoid button style duplication"""
+    base_classes = "bg-[#1DB0CD] hover:bg-[#19a0bb] text-white font-medium py-3 px-3 rounded-md transition duration-300 h-12 w-44 text-sm"
+    return Button(cls=f"{base_classes} {additional_classes}")("Join Meditate for India")
+
 
 @rt("/")
 def get_homepage():
     return Main(
-        Div(cls="flex flex-col items-center justify-center relative")(
-            Img(cls="max-w-lg mx-auto md:max-w-5xl md:max-h-auto")(src='static/img/meditation.png'),
-            H1(cls="absolute text-center text-4xl top-1 md:text-5xl md:top-10 md:mt-14 md:-ml-12")("Meditate for India"),
-            Div(cls="absolute flex flex-col items-center justify-center mt-[11.5rem] md:mt-80 text-center")(
-                P(cls="text-xl text-teal-800 text-center font-sans max-w-prose leading-loose")
+        Div(cls="mt-8 flex flex-col items-center justify-center relative")(
+            Img(cls="md:max-w-3xl md:max-h-auto")(src='static/img/meditation.png'),
+            H1(cls="absolute text-center text-3xl top-1 md:text-5xl md:top-6 md:mt-10 md:-mr-12")("Meditate for India"),
+            Div(cls="absolute flex flex-col text-sm items-center justify-center mt-[9rem] md:mt-80 md:text-xl text-center")(
+                P(cls="text-teal-800 text-center font-sans max-w-prose leading-relaxed md:mt-10")
                     (
-                    Span(cls="block font-heading")("A Global 24-Hour Immersion in"),
-                    Span(cls="block font-heading")("Meditation, Pranayama and Chanting"),
-                    Span(cls="block font-bold")("21st June 2025 From: time")
+                    Span(cls="block font-heading")("A Free Global 24-Hour Immersion in"),
+                    Span(cls="block font-heading")("Meditation, Pranayama, Asana and Chanting"),
+                    Span(cls="block font-bold pt-1 md:pt-4")("21st June 2025 From: time")
                 ),
+                meditation_button("hidden md:block mt-4 md:text-base md:w-52")
             ),
-            Button(
-                cls="bg-[#1DB0CD] hover:bg-[#19a0bb] text-white font-medium py-3 px-6 rounded-md "
-                    "transition duration-300 h-12;")
-            ("Join Meditate for India"),
+
+            meditation_button("block md:hidden mt-4"),
+
 
         ),
 
