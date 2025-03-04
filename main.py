5e279dc97db972537cb9ee1cee717ac33dd9afcd
Pavan Tikkani <pavan@Pavans-MacBook-Pro.local>
2025-03-04 21:50:32 +0530

    Correxted styling

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def meditation_button(additional_classes=""):
 @rt("/")
 def get_homepage():
     return Main(
-        Div(cls="mt-8 flex flex-col items-center justify-center relative")(
+        Section(cls="mt-16 relative w-full flex flex-col items-center justify-center")(
             Img(cls="md:max-w-3xl md:max-h-auto")(src='static/img/meditation.png'),
-            H1(cls="absolute text-center text-3xl top-1 md:text-5xl md:top-6 md:mt-10 md:-mr-12")("Meditate for India"),
+            H1(cls="absolute text-center text-4xl -top-5 ml-3.5 md:text-5xl md:top-6 md:mt-10 md:-mr-12")("Meditate for India"),
             Div(cls="absolute flex flex-col text-sm items-center justify-center mt-[9rem] md:mt-80 md:text-xl text-center")(
                 P(cls="text-teal-800 text-center font-sans max-w-prose leading-relaxed md:mt-10")
                     (
@@ -32,15 +32,30 @@ def get_homepage():
                 ),
                 meditation_button("hidden md:block mt-4 md:text-base md:w-52")
             ),
-
             meditation_button("block md:hidden mt-4"),
-
-
         ),
 
+        Section(cls="mt-16 w-full flex items-center justify-center")(
+            Div(cls="flex justify-center")(
+                Div(cls="flex flex-col items-center justify-center mx-6")(
+                    Img(cls="md:w-14 md:m-8 w-6")(src='static/img/imagine.svg'),
+                    P(cls="text-xs md:text-xl text-center text-slate-700")("Reimagining a Mindful India"),
+                    P(cls="text-xs m-2 md:text-lg text-slate-600 text-center")("पुनर्निर्मिं मः मनस्वी भारतम्")
+                ),
+                Div(cls="flex flex-col items-center justify-center mx-6")(
+                    Img(cls="md:w-14 md:m-8 w-6")(src='static/img/celebrate.svg'),
+                    P(cls="text-xs md:text-xl text-center text-slate-700")("Celebrating Our Heritage"),
+                    P(cls="text-xs md:text-lg text-slate-600 text-center m-2")("उत्सवामः स्वधर्म मंम")
+                ),
+                Div(cls="flex flex-col items-center justify-center mx-6")(
+                    Img(cls="md:w-14 md:m-8 w-6")(src='static/img/cultivate.svg'),
+                    P(cls=" text-xs md:text-xl text-center text-slate-700")("Cultivating Inner Wisdom"),
+                    P(cls="text-xs md:text-lg text-slate-600 text-center m-2")("पालयामः अन्तःप्रज्ञाम्")
+                ),
 
+            )
+        ),
 
     )
-
 
 serve()
